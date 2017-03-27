76203ca7867d70519b6053466cbea8fe71f47698
James <jameslabroy@gmail.com>
2017-03-27 23:35:16 +0100

    fixing up the server

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+const compression = require('compression');
 const express = require('express');
+const fs = require('fs');
+const helmet = require('helmet');
+const https = require('https');
+const morgan = require('morgan');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static('./build', {extensions:['html']}));
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(compression());
+app.use(express.static('./build', {extensions: ['html']}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000);
+const port = 9000;
+const key = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const credentials = {key: key, cert: certificate}
+
+const server = https.createServer(credentials, app);
+server.listen(port, function() {
+  console.log('Server Running on port %s', port);
+});
